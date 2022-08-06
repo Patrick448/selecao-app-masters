fcd8679bdfc9be4fecbfc8ba55a8755b899e7e76
Patrick448 <patrickcarvalho448@gmail.com>
2022-08-06 20:15:22 -0300

    Instituições e ajustes

--- doar-computadores/pages/index.js
+++ doar-computadores/pages/index.js
@@ -3,6 +3,8 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import axios from 'axios'
 import { useState, useEffect } from 'react';
+import Link from 'next/link';
+
 
 const apiAddess = "https://api-doar-computadores.herokuapp.com"
 const donationRoute = apiAddess + "/donation"
@@ -59,21 +61,21 @@ export default function Home() {
   const [error, setError] = useState(false);
   const [loadingZip, setLoadingZip] = useState(false);
   const [zipError, setZipError] = useState(false);
-  const [formData, setFormData] = useState(
+  const [formState, setFormState] = useState(
     {
-    name: "",
-    email: "",
-    phone: "",
-    zip: "",
-    city: "",
-    state: "",
-    streetAddress: "",
-    number: "",
-    complement: "",
-    neighborhood: "",
-    deviceCount: "",
-    devices: []
-  })
+      name: { value: "", editted: false, valid: false },
+      email: { value: "", editted: false, valid: false },
+      phone: { value: "", editted: false, valid: false },
+      zip: { value: "", editted: false, valid: false },
+      city: { value: "", editted: false, valid: false },
+      state: { value: "", editted: false, valid: false },
+      streetAddress: { value: "", editted: false, valid: false },
+      number: { value: "", editted: false, valid: false },
+      complement: { value: "", editted: false, valid: false },
+      neighborhood: { value: "", editted: false, valid: false },
+      deviceCount: { value: "", editted: false, valid: false },
+      devices: []
+    })
 
   const types = [
     { displayName: "Notebook", id: "notebook" },
@@ -120,37 +122,18 @@ export default function Home() {
     console.log(deviceList)
   }
 
-  const DeviceDataForm = (props) => {
-    let donation = props.item
-    let index = props.index
-    console.log("Building form item")
-    console.log(donation)
 
-    return <div className="box-1">
-
-      <div className='label-input-field'>
-        <label htmlFor="type">Tipo de equipamento</label>
-        <select required defaultValue="none" value={donation.type == null ? "none" : donation.type} name="type" id="type" form="donation-form" onChange={(e) => onDeviceFormItemChange(e, index)}>
-          <option disabled value="none"> -- selecione uma opção -- </option>
-          {types.map((item) => <option key={"type" + item.id + index} value={item.id}>{item.displayName}</option>)}
-        </select>
-      </div>
-      <div className='label-input-field'>
-
-        <label htmlFor="condition">Condição</label>
-        <select required defaultValue="none" value={donation.condition == null ? "none" : donation.condition} name="condition" id="condition" form="donation-form" onChange={(e) => onDeviceFormItemChange(e, index)}>
-          <option disabled value="none"> -- selecione uma opção -- </option>
-          {conditions.map((item) => <option key={"cond" + item.id + index} value={item.id}>{item.displayName}</option>)}
-        </select>
-      </div>
-    </div>
-  }
 
   const loadZipDataToForm = (data) => {
-    document.getElementById("state").value = data.uf
-    document.getElementById("neighborhood").value = data.bairro
-    document.getElementById("city").value = data.localidade
-    document.getElementById("streetAddress").value = data.logradouro
+
+    const newFormState = { ...formState };
+
+    newFormState.state = { value: data.uf, editted: true, valid: true }
+    newFormState.neighborhood = { value: data.bairro, editted: true, valid: true }
+    newFormState.city = { value: data.localidade, editted: true, valid: true }
+    newFormState.streetAddress = { value: data.logradouro, editted: true, valid: true }
+
+    setFormState(newFormState)
     document.getElementById("number").focus()
 
   }
@@ -178,20 +161,20 @@ export default function Home() {
 
   const handleSubmit = (event) => {
     event.preventDefault()
-
+    checkFieldsValid()
 
     const data = {
-      name: event.target.name.value,
-      email: event.target.email.value,
-      phone: event.target.phone.value,
-      zip: event.target.zip.value,
-      city: event.target.city.value,
-      state: event.target.state.value,
-      streetAddress: event.target.streetAddress.value,
-      number: event.target.number.value,
-      complement: event.target.complement.value,
-      neighborhood: event.target.neighborhood.value,
-      deviceCount: parseInt(deviceCount),
+      name: formState.name.value,
+      email: formState.email.value,
+      phone: formState.phone.value,
+      zip: formState.zip.value,
+      city: formState.city.value,
+      state: formState.state.value,
+      streetAddress: formState.streetAddress.value,
+      number: formState.number.value,
+      complement: formState.complement.value,
+      neighborhood: formState.neighborhood.value,
+      deviceCount: parseInt(formState.deviceCount.value),
       devices: deviceList
     }
 
@@ -204,8 +187,55 @@ export default function Home() {
         setMessage(e.message);
         setError(true)
       })
-    getZipApi(event.target.zip.value)
+    getZipApi(formState.zip.value)
   }
+
+  //valida campos do formulário de dados pessoais
+  //o patern decide se o formato está correto, o filter verifica cada caracter, permitindo a inserção ou não
+  const handleFormChange = (event) => {
+    console.log(event)
+    const inputName = event.target.name;
+    const inputValue = event.target.value;
+    const newFormState = { ...formState };
+
+    const validations = {
+      email: { pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, filter: /.*/ },
+      phone: { pattern: /^[0-9]{10,11}$/, filter: /^[0-9]*$/ },
+      zip: { pattern: /^[0-9]{5}\-?[0-9]{3}$/, filter: /^[0-9]*\-?[0-9]*$/ },
+      number: { pattern: /^[0-9]*$/, filter: /^[0-9]*$/ },
+      deviceCount: { pattern: /^[0-9]*$/, filter: /^[0-9]*$/ }
+    }
+
+    if (validations[inputName] != undefined) {
+
+      if (!validations[inputName].filter.test(inputValue)) {
+        console.log(inputValue + " typed")
+        return;
+      }
+
+      console.log(validations[inputName]);
+      const valid = validations[inputName].pattern.test(inputValue);
+      console.log("valid " + inputName + ": " + valid);
+      newFormState[inputName].valid = valid;
+    }
+
+    newFormState[inputName].value = inputValue;
+    newFormState[inputName].editted = true;
+
+    setFormState(newFormState)
+
+    console.log(newFormState)
+
+  }
+
+  const checkFieldsValid =()=>{
+
+    for (const item in Object.entries(formState)) {
+      console.log(item)
+    }
+  }
+
+  
 
   return (
     <div className='container'>
@@ -213,45 +243,66 @@ export default function Home() {
         <Image src="/computer-heart-icon.svg" height={100} width={100} />
         <div className='header-text'>
           <h1>Doação de Computadores Usados</h1>
-          {isApiAlive ? <p>API Online</p> : <p>API Offline</p>}
+          <div className='header-sub-text row'>
+            <div className='header-menu-item'>
+              {isApiAlive ? <p>API Online</p> : <p>API Offline</p>}
+            </div>
+            <div className='header-menu-item'>
+              <p>
+                <Link href="/institutions">
+                  <a>Instituições</a>
+                </Link>
+              </p>
+            </div>
+          </div>
+
         </div>
+
       </div>
-      <form id="donation-form" onSubmit={handleSubmit}>
+      <form id="donation-form" onSubmit={handleSubmit} onChange={handleFormChange}>
         <div className='form-container'>
           <div className="box-1">
-            <h2>Dados do doador</h2>
+            <h2 className='form-header'>Dados do doador</h2>
 
             <div className='label-input-field'>
               <label htmlFor="name">Nome</label>
-              <input type="text" id="name" name="name" required></input>
+              <input type="text" id="name" name="name" placeholder="Seu nome completo" value={formState.name.value} required></input>
             </div>
+            
 
+            <FormField name="name" label="Nome" placeholder="Digite aqui seu nome" required={true} value={formState.name.value} />
             <div className='label-input-field'>
-              <label htmlFor="email">E-mail</label>
-              <input type="text" id="email" name="email" pattern='/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/'></input>
+              <label htmlFor="email">E-mail (opcional)</label>
+              <input type="text" id="email" name="email" placeholder="Seu endereço de e-mail" value={formState.email.value} />
+              <div className='error'>{formState.email.editted && !formState.email.valid ? "E-mail inválido" : ""}</div>
             </div>
 
             <div className='label-input-field'>
               <label htmlFor="phone">Telefone</label>
-              <input type="text" id="phone" name="phone" required></input>
+              <input type="text" id="phone" name="phone" placeholder='Seu número de telefone (apenas dígitos)' required value={formState.phone.value}></input>
+              <div className='error'>{formState.phone.editted && !formState.phone.valid ? "Número de telefone inválido" : ""}</div>
+
             </div>
 
-            <div className='label-input-field'>
-              <label htmlFor="zip">CEP</label>
-              <input type="text" maxLength={9} id="zip" name="zip" onChange={handleZipChange} required></input>
-            </div>
+            <FormField
+              label="CEP"
+              name="zip"
+              error={(zipError || (formState.zip.editted && !formState.zip.valid))}
+              placeholder=""
+              value={formState.zip.value}
+              errorMessage="CEP inválido"
+              onChange={handleFormChange} />
 
             <div className='success'>{loadingZip ? "Carregando endereço" : ""}</div>
-            <div className='error'>{zipError ? "CEP inválido" : ""}</div>
 
             <div className='formSection'>
               <div className='fill label-input-field'>
                 <label htmlFor="city">Cidade</label>
-                <input type="text" id="city" name="city" required></input>
+                <input type="text" id="city" name="city" required value={formState.city.value}></input>
               </div>
               <div className='fill label-input-field'>
                 <label htmlFor="state">Estado</label>
-                <input type="text" id="state" name="state" required></input>
+                <input type="text" id="state" name="state" required value={formState.state.value}></input>
               </div>
 
             </div>
@@ -260,53 +311,106 @@ export default function Home() {
 
               <div className='fill label-input-field'>
                 <label htmlFor="streetAddress">Nome do logradouro</label>
-                <input type="text" id="streetAddress" name="streetAddress" required></input>
+                <input type="text" id="streetAddress" name="streetAddress" required value={formState.streetAddress.value}></input>
               </div>
 
               <div className='fill label-input-field'>
                 <label htmlFor="number">Número</label>
-                <input type="text" id="number" name="number" required></input>
+                <input type="text" id="number" name="number" required value={formState.number.value}></input>
+                <div className='error'>{formState.number.editted && !formState.number.valid ? "Número inválido" : ""}</div>
+
               </div>
 
             </div>
 
             <div className='label-input-field'>
-              <label htmlFor="complement">Complemento</label>
-              <input type="text" id="complement" name="complement"></input>
+              <label htmlFor="complement">Complemento (opcional)</label>
+              <input type="text" id="complement" name="complement" value={formState.complement.value}></input>
             </div>
 
             <div className='label-input-field'>
               <label htmlFor="neighborhood">Bairro</label>
-              <input type="text" id="neighborhood" name="neighborhood" required></input>
+              <input type="text" id="neighborhood" name="neighborhood" required value={formState.neighborhood.value}></input>
             </div>
-
-
 
             <div className='formSection'>
 
-              <div className='label-input-field fill'>
+              <div className='label-input-field'>
                 <label htmlFor="deviceCount">Número de doações</label>
-                <input type="number" onChange={handleDeviceCountChange} id="deviceCount" name="deviceCount" required />
+                <input type="text" id="deviceCount" name="deviceCount" value={formState.deviceCount.value} required />
+                <div className='error'>{formState.deviceCount.editted && !formState.deviceCount.valid ? "Quantidade inválida" : ""}</div>
+
               </div>
               <div className='label-input-field'>
-                <button type="button" onClick={() => showDevicesForm(deviceCount)}>Continuar</button>
+                <button type="button" onClick={() => showDevicesForm(formState.deviceCount.value)}>Continuar</button>
               </div>
             </div>
 
           </div>
-        </div>
+        </div> </form>
+      <form>
         <div className={`form-container ${deviceList.length > 0 ? "show" : "hide"}`}>
-          <div className='sub-header-container'>
-            <h2>Dados das doações</h2>
-          </div>
-          {deviceList.map((item, index) => { console.log("mapped: " + item); return <DeviceDataForm key={index} item={item} index={index} /> })}
-          <p className={`message ${error ? "error" : "success"}`}>{message}</p>
-          <button type="submit">Enviar</button>
-        </div>
 
+          <div className='box-1'>
+
+            <div className='sub-header-container'>
+              <h2>Dados das doações</h2>
+            </div>
+            {deviceList.map((item, index) =>
+              <DeviceDataForm
+                types={types}
+                conditions={conditions}
+                onChange={onDeviceFormItemChange}
+                key={index}
+                item={item}
+                index={index} />
+            )}
+            <p className={`message ${error ? "error" : "success"}`}>{message}</p>
+            <button type="submit" form="donation-form" onSubmit={handleSubmit}>Enviar</button>
+          </div>
+        </div>
       </form>
+
+
+
     </div>
   )
 }
 
+const DeviceDataForm = (props) => {
+  let types = props.types
+  let conditions = props.conditions
+  let donation = props.item
+  let index = props.index
+  console.log("Building form item")
+  console.log(donation)
 
+  return <div className="device-data-box">
+
+    <div className='label-input-field'>
+      <label htmlFor="type">Tipo de equipamento</label>
+      <select required defaultValue="none" value={donation.type == null ? "none" : donation.type} name="type" id="type" form="donation-form" onChange={(e) => props.onChange(e, index)}>
+        <option disabled value="none"> -- selecione um tipo -- </option>
+        {types.map((item) => <option key={"type" + item.id + index} value={item.id}>{item.displayName}</option>)}
+      </select>
+    </div>
+    <div className='label-input-field'>
+
+      <label htmlFor="condition">Condição</label>
+      <select required defaultValue="none" value={donation.condition == null ? "none" : donation.condition} name="condition" id="condition" form="donation-form" onChange={(e) => props.onChange(e, index)}>
+        <option disabled value="none"> -- selecione uma condição -- </option>
+        {conditions.map((item) => <option key={"cond" + item.id + index} value={item.id}>{item.displayName}</option>)}
+      </select>
+    </div>
+  </div>
+}
+
+const FormField = (props) => {
+  //console.log(props)
+
+  return <div className='label-input-field'>
+    <label htmlFor={props.name}>{props.label}</label>
+    <input type="text" id={props.name} name={props.name} placeholder={props.placeholder} value={props.value} required={props.required} onChange={props.onChange} />
+    <div className='error'>{props.error == true ? props.errorMessage : ""}</div>
+  </div>
+}
---
+++ doar-computadores/pages/institutions.js
@@ -0,0 +1,114 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from '../styles/Home.module.css'
+import axios from 'axios'
+import { useState, useEffect } from 'react';
+
+
+export default function InstitutionsList() {
+
+    const institutions = [{
+        name: "Escola de Informática",
+        city: "Juiz de Fora",
+        neighborhood: "Centro",
+        presentation: "Este projeto é voltado para toda a comunidade, sendo ela carente, com o objetivo principal da inserção desses alunos no mundo digital.  Como a informática é uma ferramenta imprescindível ao trabalho, à comunicação e ao próprio modo de vida contemporânea o seu domínio resulta em uma inserção social mais ampla. Por isso, executamos ações que visam a ampliar o acesso das camadas populares ao mundo digital e que possibilitem esta clientela ingressar no mercado de trabalho. ",
+        contact: { website: "http://einfojf.com.br", instagram: "http://instagram.com", facebook: "http://facebook.com", whatsapp: "http://wa.me/32912345678" }
+
+    },
+    {
+        name: "InfoCenter",
+        city: "Rio de Janeiro",
+        neighborhood: "Cordovil",
+        presentation: "Este projeto é voltado para toda a comunidade, sendo ela carente, com o objetivo principal da inserção desses alunos no mundo digital.  Como a informática é uma ferramenta imprescindível ao trabalho, à comunicação e ao próprio modo de vida contemporânea o seu domínio resulta em uma inserção social mais ampla. Por isso, executamos ações que visam a ampliar o acesso das camadas populares ao mundo digital e que possibilitem esta clientela ingressar no mercado de trabalho. ",
+        contact: { website: "einfojf.com.br", instagram: "@eijf", facebook: "EIJF", whatsapp: "(32) 9 1234-5678" }
+
+    },
+    {
+        name: "Projeto Social PC Terê",
+        city: "Teresópolis",
+        neighborhood: "Várzea",
+        presentation: "Este projeto é voltado para toda a comunidade, sendo ela carente, com o objetivo principal da inserção desses alunos no mundo digital.  Como a informática é uma ferramenta imprescindível ao trabalho, à comunicação e ao próprio modo de vida contemporânea o seu domínio resulta em uma inserção social mais ampla. Por isso, executamos ações que visam a ampliar o acesso das camadas populares ao mundo digital e que possibilitem esta clientela ingressar no mercado de trabalho. ",
+        contact: { website: "einfojf.com.br", instagram: "@eijf", facebook: "EIJF", whatsapp: "(32) 9 1234-5678" }
+
+    },
+    {
+        name: "Escola de Informática 2",
+        city: "Juiz de Fora",
+        neighborhood: "Granbery",
+        presentation: "Este projeto é voltado para toda a comunidade, sendo ela carente, com o objetivo principal da inserção desses alunos no mundo digital.  Como a informática é uma ferramenta imprescindível ao trabalho, à comunicação e ao próprio modo de vida contemporânea o seu domínio resulta em uma inserção social mais ampla. Por isso, executamos ações que visam a ampliar o acesso das camadas populares ao mundo digital e que possibilitem esta clientela ingressar no mercado de trabalho. ",
+        contact: { website: "einfojf.com.br", instagram: "@eijf", facebook: "EIJF", whatsapp: "(32) 9 1234-5678" }
+
+    },
+    {
+        name: "Escola de Informática 3",
+        city: "Juiz de Fora",
+        neighborhood: "São Pedro",
+        presentation: "Este projeto é voltado para toda a comunidade, sendo ela carente, com o objetivo principal da inserção desses alunos no mundo digital.  Como a informática é uma ferramenta imprescindível ao trabalho, à comunicação e ao próprio modo de vida contemporânea o seu domínio resulta em uma inserção social mais ampla. Por isso, executamos ações que visam a ampliar o acesso das camadas populares ao mundo digital e que possibilitem esta clientela ingressar no mercado de trabalho. ",
+        contact: { website: "einfojf.com.br", instagram: "@eijf", facebook: "EIJF", whatsapp: "(32) 9 1234-5678" }
+
+    }
+    ]
+
+
+    const Card = (props) => {
+        return <div className="box-1">
+            <h2>{props.name}</h2>
+            <p>{props.neighborhood}, {props.city}</p>
+            <div className='divider'></div>
+            <p>{props.presentation}</p>
+            <div className='social-links-box row'>
+
+                <div><p><a target="_blank" href={props.website}>Site</a></p></div>
+                <div><p><a target="_blank"  href={props.instagram}>Instagram</a></p></div>
+                <div><p><a target="_blank"  href={props.facebook}>Facebook</a></p></div>
+                <div><p><a target="_blank"  href={props.whatsapp}>Whatsapp</a></p></div>
+            </div>
+        </div>
+    }
+
+
+    return (
+        <div className='container'>
+            <div className='header'>
+                <Image src="/computer-heart-icon.svg" height={100} width={100} />
+                <div className='header-text'>
+                    <h1>Lista de Instituições</h1>
+                    <div className='header-sub-text row'>
+
+                        <div className='header-menu-item'>
+                            <p>
+                                <Link href="/">
+                                    <a>{"<"} Voltar ao formulário</a>
+                                </Link>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+            </div>
+
+            <div className='form-container'>
+                {institutions.map(
+                    (item) =>
+
+                        <Card
+                            name={item.name}
+                            city={item.city}
+                            neighborhood={item.neighborhood}
+                            presentation={item.presentation}
+                            website={item.contact.website}
+                            instagram={item.contact.instagram}
+                            facebook={item.contact.facebook}
+                            whatsapp={item.contact.whatsapp}
+                        />
+                )}
+
+
+            </div>
+        </div>
+    )
+}
+
+
